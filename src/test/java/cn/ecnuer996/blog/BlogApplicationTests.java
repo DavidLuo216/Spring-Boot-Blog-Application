0768265e5a0e2be7833cbcc2cf350c282ff42119
Lu Wen Qing <david1113531523@gmail.com>
2020-02-15 15:51:03 +0800

    first commit

---
+++ src/test/java/cn/ecnuer996/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ecnuer996.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
